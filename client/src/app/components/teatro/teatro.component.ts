a2e00039a568c4a86dec2d7a38025043fcb355c7
DaanMoura <daniel.lucio.moura@gmail.com>
2019-07-09 11:02:09 -0300

    criando tabela para salaTeatro

--- client/src/app/components/teatro/teatro.component.ts
+++ client/src/app/components/teatro/teatro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { SalaTeatro } from 'src/app/models/salaTeatro';
 
 @Component({
   selector: 'app-teatro',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeatroComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'nome', 'cidade', 'cnpj'];
+  salas: SalaTeatro[] = [];
+  isLoadingResults = true;
+
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+      // this.getData();
+      this.api.getSalaTeatros().subscribe(res => {
+        this.salas = res;
+        console.log(this.salas);
+        this.isLoadingResults = false;
+      }, err=> {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
+
+  // async getData() {
+  //   this.salas = await this.api.getSalaTeatros().toPromise();
+  //   this.isLoadingResults = false;
+  //   console.debug('Waiting for promisse get resolved...');
+  // }
 
 }
